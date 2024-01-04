ceff4a2391e51ce2f92073c8bf66f87347db7f93
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2024-01-04 18:50:01 +0800

    Finish rps logic by refactoring if-statement logic

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let playerPoint = 1;
-let computerPoint = 1;
+let playerPoint = 0;
+let computerPoint = 0;
+const buttons = document.querySelectorAll("button");
 
 function getComputerChoice() {
     let randomComputerChoice = Math.floor(Math.random() * 3) + 1;
@@ -7,83 +8,81 @@ function getComputerChoice() {
 
     switch (randomComputerChoice) {
         case 1:
-            computerChoice = "Rock";
+            computerChoice = "rock";
             break;
         case 2:
-            computerChoice = "Paper";
+            computerChoice = "paper";
             break;
         case 3:
-            computerChoice = "Scissors";
+            computerChoice = "scissors";
             break;
     }
 
     return computerChoice;
 }
 
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
 const divContent = document.createElement("div");
-const runningComputerScore = document.querySelector(".runningComputerScore");
-const runningPlayerScore = document.querySelector(".runningPlayerScore");
 const playerScore = document.createElement("int");
 const computerScore = document.createElement("int");
-
-
+const rpsWinner = document.createElement("p");
 
 function playRound(playerSelection, computerSelection) {
 
-    let caseinsensitivePlayer = playerSelection.toLowerCase();
-    
-    
-    if (caseinsensitivePlayer === "rock" && computerSelection === "Rock") {
-        divContent.textContent = "It's a tie!";
-    } else if (caseinsensitivePlayer === "rock" && computerSelection == "Paper") {
-        divContent.textContent = "You lose! Paper beats Rock";
-        computerScore.textContent = computerPoint++;
-    } else if (caseinsensitivePlayer === "rock" && computerSelection == "Scissors") {
-        divContent.textContent = "You win! Rock beats Scissors";  
-        playerScore.textContent = playerPoint++;
-    } else if (caseinsensitivePlayer === "paper" && computerSelection == "Rock") {
-        divContent.textContent = "You win! Paper beats Rock";
-        playerScore.textContent = playerPoint++;
-    } else if (caseinsensitivePlayer === "paper" && computerSelection == "Paper") {
-        divContent.textContent = "It's a tie!";
-    } else if (caseinsensitivePlayer === "paper" && computerSelection == "Scissors") {
-        divContent.textContent = "You lose! Scissors beats Paper";
-       computerScore.textContent = computerPoint++;
-    } else if (caseinsensitivePlayer === "scissors" && computerSelection == "Rock") {
-        divContent.textContent = "You lose! Rock beats Scissors";
-        computerScore.textContent = computerPoint++;
-    } else if (caseinsensitivePlayer === "scissors" && computerSelection == "Paper") {
-        divContent.textContent = "You win! Scissors beats Paper";
-        playerScore.textContent = playerPoint++;
-    } else if (caseinsensitivePlayer === "scissors" && computerSelection == "Scissors") {
-        divContent.textContent = "It's a tie!";
+    if ((playerSelection === "rock" && computerSelection === "scissors") || 
+    (playerSelection === "scissors" && computerSelection === "paper") || 
+    (playerSelection === "paper" && computerSelection === "rock")) {
+
+        playerPoint++
+        divContent.textContent = `You win! ${playerSelection} beats ${computerSelection}!`
+        playerScore.textContent = playerPoint;
+
+        if (playerPoint === 5) {
+            rpsWinner.textContent = "You won the game! Reload the page to play again."
+            disableButtons();
+        }
+    } else if (playerSelection === computerSelection){
+        divContent.textContent = `It's a tie, you both chose ${playerSelection}`;
     } else {
-        divContent.textContent = "Invalid input, try again!";
+        computerPoint++
+        divContent.textContent = `You lose! ${computerSelection} beats ${playerSelection}!`
+        computerScore.textContent = computerPoint;
+
+        if (computerPoint === 5) {
+            rpsWinner.textContent = `The computer has won the game! Reload the page to play again.`;
+            disableButtons()
+        }
     }
-
-
 }
-
-
 
 const rockBtn = document.querySelector(".rockBtn");
 const paperBtn = document.querySelector(".paperBtn");
 const scissorsBtn = document.querySelector(".scissorsBtn");
 const rpsResults = document.querySelector(".rpsResults");
+const gameWinner = document.querySelector(".gameWinner");
+
+const runningComputerScore = document.querySelector(".runningComputerScore");
+const runningPlayerScore = document.querySelector(".runningPlayerScore");
 
 
 rockBtn.addEventListener("click", () => {
-    playRound("Rock", getComputerChoice());
+    playRound("rock", getComputerChoice());
 })
 
 paperBtn.addEventListener("click", () => {
-    playRound("Paper", getComputerChoice());
+    playRound("paper", getComputerChoice());
 })
 
 scissorsBtn.addEventListener("click", () => {
-    playRound("Scissors", getComputerChoice());
+    playRound("scissors", getComputerChoice());
 })
 
 rpsResults.appendChild(divContent);
 runningPlayerScore.append(playerScore);
 runningComputerScore.append(computerScore);
+gameWinner.append(rpsWinner);
